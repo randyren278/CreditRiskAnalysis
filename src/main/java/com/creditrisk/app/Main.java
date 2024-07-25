abb5487cef15d7106ee7ef555b009a990d0dc332
randyren278 <randyren278@gmail.com>
2024-07-24 20:20:21 -0600

    main + data base connector

---
+++ src/main/java/com/creditrisk/app/Main.java
@@ -0,0 +1,9 @@
+package com.creditrisk.app;
+
+import com.creditrisk.gui.MainUI;
+
+public class Main {
+    public static void main(String[] args) {
+        MainUI.launch(MainUI.class, args);
+    }
+}
